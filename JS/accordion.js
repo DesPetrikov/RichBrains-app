a7a57f460e22f4b30d94795051d25ce1ab2dd1f5
Dzianis Petrykau <desik1603@gmail.com>
2023-02-02 15:30:51 +0300

    feat: header, accordion with template were implemented

---
+++ JS/accordion.js
@@ -0,0 +1,104 @@
+const accordion = {
+  titleDesktop: 'CAREERS',
+  titleMobile: 'CHOOSE YOUR CAREER',
+  titleIcon: '../assets/icons/education.svg',
+  careerList: [
+    {
+      id: 'career-1',
+      title: 'Business & Management',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+    {
+      id: 'career-2',
+      title: 'Computing & IT',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+    {
+      id: 'career-3',
+      title: 'Fashion & Media',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+    {
+      id: 'career-4',
+      title: 'Finance & Accounting',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+    {
+      id: 'career-5',
+      title: 'Law & Criminology',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+    {
+      id: 'career-6',
+      title: 'Marketing & <br> Communications',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+    {
+      id: 'career-7',
+      title: 'Psychology & Social <br> Sciences',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+    {
+      id: 'career-8',
+      title: 'Business & Management',
+      subtitles: [
+        { subtitle: 'Market Research' },
+        { subtitle: 'Executive Child Psychologist' },
+        { subtitle: 'Guidance Counsellor' },
+        { subtitle: 'Behaviour Analyst' },
+        { subtitle: 'Recreational Therapist' },
+      ],
+    },
+  ],
+
+  mainBtnTitle: 'show all programmes',
+};
+
+const template = Handlebars.compile(
+  document.getElementById('accordion-template').innerHTML
+);
+
+const filled = template(accordion, { noEscape: true });
+document.getElementById('accordion').innerHTML = filled;
