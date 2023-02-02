2dbbc8687e1a9804690984be922493d6ad9742dd
Dzianis Petrykau <desik1603@gmail.com>
2023-02-02 16:24:30 +0300

    feat: the data for cards was added

---
+++ JS/cards.js
@@ -0,0 +1,155 @@
+const cards = {
+  cardsList: [
+    {
+      id: 'card-1',
+      frontSide: {
+        logo: '../assets/icons/embratur.svg',
+        category: 'Business & Management',
+        profession: 'Master of Business Administration (CMI)',
+		  educationLevel: 'Postgraduate',
+		  frequency: 'Every 12 weeks ',
+		  buttonTitle: 'More Info',
+      },
+		backSide: {
+			profession: 'MSc Data Analytics and Information Systems Management',
+			duration: {
+				title: 'Duration:',
+				min: 'Minimum: 18 months',
+				max: 'Maximum: 36 months',
+			},
+			start: {
+				title: 'Start Dates:',
+				dates: 'January, April, July, October'
+			},
+			cost: '£8,500',
+			buttonTitle: 'More Info',
+		}
+    },
+	 {
+      id: 'card-2',
+      frontSide: {
+        logo: '../assets/icons/embratur.svg',
+        category: 'Business & Management',
+        profession: 'Master of Business Administration (CMI)',
+		  educationLevel: 'Postgraduate',
+		  frequency: 'Every 12 weeks ',
+		  buttonTitle: 'More Info',
+      },
+		backSide: {
+			profession: 'MSc Data Analytics and Information Systems Management',
+			duration: {
+				title: 'Duration:',
+				min: 'Minimum: 18 months',
+				max: 'Maximum: 36 months',
+			},
+			start: {
+				title: 'Start Dates:',
+				dates: 'January, April, July, October'
+			},
+			cost: '£8,500',
+			buttonTitle: 'More Info',
+		}
+    },
+	 {
+      id: 'card-3',
+      frontSide: {
+        logo: '../assets/icons/danisco.svg',
+        category: 'Business & Management',
+        profession: 'Master of Business Administration (CMI)',
+		  educationLevel: 'Postgraduate',
+		  frequency: 'Every 12 weeks ',
+		  buttonTitle: 'More Info',
+      },
+		backSide: {
+			profession: 'MSc Data Analytics and Information Systems Management',
+			duration: {
+				title: 'Duration:',
+				min: 'Minimum: 18 months',
+				max: 'Maximum: 36 months',
+			},
+			start: {
+				title: 'Start Dates:',
+				dates: 'January, April, July, October'
+			},
+			cost: '£8,500',
+			buttonTitle: 'More Info',
+		}
+    },
+	 {
+      id: 'card-4',
+      frontSide: {
+        logo: '../assets/icons/danisco.svg',
+        category: 'Business & Management',
+        profession: 'Master of Business Administration (CMI)',
+		  educationLevel: 'Postgraduate',
+		  frequency: 'Every 12 weeks ',
+		  buttonTitle: 'More Info',
+      },
+		backSide: {
+			profession: 'MSc Data Analytics and Information Systems Management',
+			duration: {
+				title: 'Duration:',
+				min: 'Minimum: 18 months',
+				max: 'Maximum: 36 months',
+			},
+			start: {
+				title: 'Start Dates:',
+				dates: 'January, April, July, October'
+			},
+			cost: '£8,500',
+			buttonTitle: 'More Info',
+		}
+    },
+	 {
+      id: 'card-5',
+      frontSide: {
+        logo: '../assets/icons/buffets.svg',
+        category: 'Business & Management',
+        profession: 'Master of Business Administration (CMI)',
+		  educationLevel: 'Postgraduate',
+		  frequency: 'Every 12 weeks ',
+		  buttonTitle: 'More Info',
+      },
+		backSide: {
+			profession: 'MSc Data Analytics and Information Systems Management',
+			duration: {
+				title: 'Duration:',
+				min: 'Minimum: 18 months',
+				max: 'Maximum: 36 months',
+			},
+			start: {
+				title: 'Start Dates:',
+				dates: 'January, April, July, October'
+			},
+			cost: '£8,500',
+			buttonTitle: 'More Info',
+		}
+    },
+	 {
+      id: 'card-6',
+      frontSide: {
+        logo: '../assets/icons/barco.svg',
+        category: 'Business & Management',
+        profession: 'Master of Business Administration (CMI)',
+		  educationLevel: 'Postgraduate',
+		  frequency: 'Every 12 weeks ',
+		  buttonTitle: 'More Info',
+      },
+		backSide: {
+			profession: 'MSc Data Analytics and Information Systems Management',
+			duration: {
+				title: 'Duration:',
+				min: 'Minimum: 18 months',
+				max: 'Maximum: 36 months',
+			},
+			start: {
+				title: 'Start Dates:',
+				dates: 'January, April, July, October'
+			},
+			cost: '£8,500',
+			buttonTitle: 'More Info',
+		}
+    },
+  ],
+  loaderTitle: 'More programmes'
+};
