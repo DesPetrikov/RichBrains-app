afb34341b76925e2fd437fa039c1c2609098a1fc
Dzianis Petrykau <desik1603@gmail.com>
2023-02-04 00:25:17 +0300

    feat: active effects for accordion and cards were added

--- JS/accordion.js
+++ JS/accordion.js
@@ -7,66 +7,39 @@ const accordion = {
       id: 'career-1',
       title: 'Business & Management',
       subtitles: [
-        { subtitle: 'Market Research Executive' },
-        { subtitle: 'Child Psychologist' },
-        { subtitle: 'Guidance Counsellor' },
-        { subtitle: 'Behaviour Analyst' },
-        { subtitle: 'Recreational Therapist' },
+        { subtitle: 'Project manager' },
+        { subtitle: 'Business analyst' },
       ],
     },
     {
       id: 'career-2',
       title: 'Computing & IT',
       subtitles: [
-        { subtitle: 'Market Research Executive' },
-        { subtitle: 'Child Psychologist' },
-        { subtitle: 'Guidance Counsellor' },
-        { subtitle: 'Behaviour Analyst' },
-        { subtitle: 'Recreational Therapist' },
+        { subtitle: 'Front-end developer' },
+        { subtitle: 'HTML-developer' },
       ],
     },
     {
       id: 'career-3',
       title: 'Fashion & Media',
-      subtitles: [
-        { subtitle: 'Market Research Executive' },
-        { subtitle: 'Executive Child Psychologist' },
-        { subtitle: 'Guidance Counsellor' },
-        { subtitle: 'Behaviour Analyst' },
-        { subtitle: 'Recreational Therapist' },
-      ],
+      subtitles: [{ subtitle: 'Stuntman' }, { subtitle: 'Film director' }],
     },
     {
       id: 'career-4',
       title: 'Finance & Accounting',
-      subtitles: [
-        { subtitle: 'Market Research Executive' },
-        { subtitle: 'Executive Child Psychologist' },
-        { subtitle: 'Guidance Counsellor' },
-        { subtitle: 'Behaviour Analyst' },
-        { subtitle: 'Recreational Therapist' },
-      ],
+      subtitles: [{ subtitle: 'Financier' }, { subtitle: 'Accountant' }],
     },
     {
       id: 'career-5',
       title: 'Law & Criminology',
-      subtitles: [
-        { subtitle: 'Market Research Executive' },
-        { subtitle: 'Child Psychologist' },
-        { subtitle: 'Guidance Counsellor' },
-        { subtitle: 'Behaviour Analyst' },
-        { subtitle: 'Recreational Therapist' },
-      ],
+      subtitles: [{ subtitle: 'Prosecutor' }, { subtitle: 'Lawyer' }],
     },
     {
       id: 'career-6',
       title: 'Marketing & <br> Communications',
       subtitles: [
-        { subtitle: 'Market Research Executive' },
-        { subtitle: 'Child Psychologist' },
-        { subtitle: 'Guidance Counsellor' },
-        { subtitle: 'Behaviour Analyst' },
-        { subtitle: 'Recreational Therapist' },
+        { subtitle: 'Marketologist' },
+        { subtitle: 'Human Resource Manager' },
       ],
     },
     {
@@ -84,9 +57,23 @@ const accordion = {
   mainBtnTitle: 'show all programmes',
 };
 
+const accordionContainer = document.getElementById('accordion');
+
 const accordionTemplate = Handlebars.compile(
   document.getElementById('accordion-template').innerHTML
 );
 
 const accordionFilled = accordionTemplate(accordion, { noEscape: true });
-document.getElementById('accordion').innerHTML = accordionFilled;
+accordionContainer.innerHTML = accordionFilled;
+
+const allProfessions = accordionContainer.querySelectorAll('.item__button');
+
+allProfessions.forEach((profession) => {
+	profession.addEventListener('click', () => {
+		if (!profession.classList.contains('item__button_active')) {
+			profession.classList.add('item__button_active');
+		 } else {
+			profession.classList.remove('item__button_active');
+		 }
+	})
+});
--- JS/cards.js
+++ JS/cards.js
@@ -6,158 +6,171 @@ const cards = {
         logo: '../assets/icons/embratur.svg',
         category: 'Business & Management',
         profession: 'Master of Business Administration (CMI)',
-		  educationLevel: 'Postgraduate',
-		  frequency: 'Every 12 weeks',
-		  buttonTitle: 'More Info',
+        educationLevel: 'Postgraduate',
+        frequency: 'Every 12 weeks',
+        buttonTitle: 'More Info',
       },
-		backSide: {
-			direction: 'MSc Data Analytics and Information Systems Management',
-			duration: {
-				title: 'Duration:',
-				min: 'Minimum: 18 months',
-				max: 'Maximum: 36 months',
-			},
-			start: {
-				title: 'Start Dates:',
-				dates: 'January, April, July, October'
-			},
-			cost: '£8,500',
-			buttonTitle: 'More Info',
-		}
+      backSide: {
+        direction: 'MSc Data Analytics and Information Systems Management',
+        duration: {
+          title: 'Duration:',
+          min: 'Minimum: 18 months',
+          max: 'Maximum: 36 months',
+        },
+        start: {
+          title: 'Start Dates:',
+          dates: 'January, April, July, October',
+        },
+        cost: '£8,500',
+        buttonTitle: 'More Info',
+      },
     },
-	 {
+    {
       id: 'card-2',
       frontSide: {
         logo: '../assets/icons/embratur.svg',
         category: 'Business & Management',
         profession: 'Master of Business Administration (CMI)',
-		  educationLevel: 'Postgraduate',
-		  frequency: 'Every 12 weeks',
-		  buttonTitle: 'More Info',
+        educationLevel: 'Postgraduate',
+        frequency: 'Every 12 weeks',
+        buttonTitle: 'More Info',
       },
-		backSide: {
-			direction: 'MSc Data Analytics and Information Systems Management',
-			duration: {
-				title: 'Duration:',
-				min: 'Minimum: 18 months',
-				max: 'Maximum: 36 months',
-			},
-			start: {
-				title: 'Start Dates:',
-				dates: 'January, April, July, October'
-			},
-			cost: '£8,500',
-			buttonTitle: 'More Info',
-		}
+      backSide: {
+        direction: 'MSc Data Analytics and Information Systems Management',
+        duration: {
+          title: 'Duration:',
+          min: 'Minimum: 18 months',
+          max: 'Maximum: 36 months',
+        },
+        start: {
+          title: 'Start Dates:',
+          dates: 'January, April, July, October',
+        },
+        cost: '£8,500',
+        buttonTitle: 'More Info',
+      },
     },
-	 {
+    {
       id: 'card-3',
       frontSide: {
         logo: '../assets/icons/danisco.svg',
         category: 'Business & Management',
         profession: 'Master of Business Administration (CMI)',
-		  educationLevel: 'Postgraduate',
-		  frequency: 'Every 12 weeks',
-		  buttonTitle: 'More Info',
+        educationLevel: 'Postgraduate',
+        frequency: 'Every 12 weeks',
+        buttonTitle: 'More Info',
       },
-		backSide: {
-			direction: 'MSc Data Analytics and Information Systems Management',
-			duration: {
-				title: 'Duration:',
-				min: 'Minimum: 18 months',
-				max: 'Maximum: 36 months',
-			},
-			start: {
-				title: 'Start Dates:',
-				dates: 'January, April, July, October'
-			},
-			cost: '£8,500',
-			buttonTitle: 'More Info',
-		}
+      backSide: {
+        direction: 'MSc Data Analytics and Information Systems Management',
+        duration: {
+          title: 'Duration:',
+          min: 'Minimum: 18 months',
+          max: 'Maximum: 36 months',
+        },
+        start: {
+          title: 'Start Dates:',
+          dates: 'January, April, July, October',
+        },
+        cost: '£8,500',
+        buttonTitle: 'More Info',
+      },
     },
-	 {
+    {
       id: 'card-4',
       frontSide: {
         logo: '../assets/icons/danisco.svg',
         category: 'Business & Management',
         profession: 'Master of Business Administration (CMI)',
-		  educationLevel: 'Postgraduate',
-		  frequency: 'Every 12 weeks',
-		  buttonTitle: 'More Info',
+        educationLevel: 'Postgraduate',
+        frequency: 'Every 12 weeks',
+        buttonTitle: 'More Info',
       },
-		backSide: {
-			direction: 'MSc Data Analytics and Information Systems Management',
-			duration: {
-				title: 'Duration:',
-				min: 'Minimum: 18 months',
-				max: 'Maximum: 36 months',
-			},
-			start: {
-				title: 'Start Dates:',
-				dates: 'January, April, July, October'
-			},
-			cost: '£8,500',
-			buttonTitle: 'More Info',
-		}
+      backSide: {
+        direction: 'MSc Data Analytics and Information Systems Management',
+        duration: {
+          title: 'Duration:',
+          min: 'Minimum: 18 months',
+          max: 'Maximum: 36 months',
+        },
+        start: {
+          title: 'Start Dates:',
+          dates: 'January, April, July, October',
+        },
+        cost: '£8,500',
+        buttonTitle: 'More Info',
+      },
     },
-	 {
+    {
       id: 'card-5',
       frontSide: {
         logo: '../assets/icons/buffets.svg',
         category: 'Business & Management',
         profession: 'Master of Business Administration (CMI)',
-		  educationLevel: 'Postgraduate',
-		  frequency: 'Every 12 weeks',
-		  buttonTitle: 'More Info',
+        educationLevel: 'Postgraduate',
+        frequency: 'Every 12 weeks',
+        buttonTitle: 'More Info',
       },
-		backSide: {
-			direction: 'MSc Data Analytics and Information Systems Management',
-			duration: {
-				title: 'Duration:',
-				min: 'Minimum: 18 months',
-				max: 'Maximum: 36 months',
-			},
-			start: {
-				title: 'Start Dates:',
-				dates: 'January, April, July, October'
-			},
-			cost: '£8,500',
-			buttonTitle: 'More Info',
-		}
+      backSide: {
+        direction: 'MSc Data Analytics and Information Systems Management',
+        duration: {
+          title: 'Duration:',
+          min: 'Minimum: 18 months',
+          max: 'Maximum: 36 months',
+        },
+        start: {
+          title: 'Start Dates:',
+          dates: 'January, April, July, October',
+        },
+        cost: '£8,500',
+        buttonTitle: 'More Info',
+      },
     },
-	 {
+    {
       id: 'card-6',
       frontSide: {
         logo: '../assets/icons/barco.svg',
         category: 'Business & Management',
         profession: 'Master of Business Administration (CMI)',
-		  educationLevel: 'Postgraduate',
-		  frequency: 'January, April, July, October, November',
-		  buttonTitle: 'More Info',
+        educationLevel: 'Postgraduate',
+        frequency: 'January, April, July, October, November',
+        buttonTitle: 'More Info',
       },
-		backSide: {
-			direction: 'MSc Data Analytics and Information Systems Management',
-			duration: {
-				title: 'Duration:',
-				min: 'Minimum: 18 months',
-				max: 'Maximum: 36 months',
-			},
-			start: {
-				title: 'Start Dates:',
-				dates: 'January, April, July, October'
-			},
-			cost: '£8,500',
-			buttonTitle: 'More Info',
-		}
+      backSide: {
+        direction: 'MSc Data Analytics and Information Systems Management',
+        duration: {
+          title: 'Duration:',
+          min: 'Minimum: 18 months',
+          max: 'Maximum: 36 months',
+        },
+        start: {
+          title: 'Start Dates:',
+          dates: 'January, April, July, October',
+        },
+        cost: '£8,500',
+        buttonTitle: 'More Info',
+      },
     },
   ],
   loaderTitle: 'More programmes',
-  loaderIcon: '../assets/icons/refresh.svg'
+  loaderIcon: '../assets/icons/refresh.svg',
 };
 
+const cardsContainer = document.getElementById('cards');
 const cardsTemplate = Handlebars.compile(
-	document.getElementById('cards-template').innerHTML
- );
- 
- const filledCards = cardsTemplate(cards);
- document.getElementById('cards').innerHTML = filledCards;
+  document.getElementById('cards-template').innerHTML
+);
+
+const filledCards = cardsTemplate(cards);
+cardsContainer.innerHTML = filledCards;
+
+const allCards = cardsContainer.querySelectorAll('.card');
+
+allCards.forEach((card, _, arr) => {
+  card.addEventListener('click', () => {
+    if (!card.classList.contains('card_active')) {
+      card.classList.add('card_active');
+    } else {
+      card.classList.remove('card_active');
+    }
+  });
+});
